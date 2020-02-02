a2250b17f0c2c6b74a88ded11fcdeacf6b8301eb
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 12:49:19 +0800

    Pretty printed the Arduino code with astyle

--- Seeed_VEML6070.h
+++ Seeed_VEML6070.h
@@ -9,9 +9,9 @@
 #define SEEED_DN_DEFINES
 
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
-  #define SERIAL_DB SerialUSB
+    #define SERIAL_DB SerialUSB
 #else
-  #define SERIAL_DB Serial
+    #define SERIAL_DB Serial
 #endif
 
 
@@ -22,22 +22,21 @@ typedef unsigned short u16;
 typedef char           s8;
 typedef unsigned char  u8;
 
-typedef enum	
-{
-    NO_ERROR=0,
-    ERROR_PARAM=-1,
-    ERROR_COMM =-2,
-    ERROR_OTHERS=-128,
-}err_t;
+typedef enum {
+    NO_ERROR = 0,
+    ERROR_PARAM = -1,
+    ERROR_COMM = -2,
+    ERROR_OTHERS = -128,
+} err_t;
 
 
 #define CHECK_RESULT(a,b)   do{if(a=b)  {    \
-                            SERIAL_DB.print(__FILE__);    \
-                            SERIAL_DB.print(__LINE__);   \
-                            SERIAL_DB.print(" error code =");  \
-                            SERIAL_DB.println(a);                   \
-                            return a;   \
-                            }}while(0)
+            SERIAL_DB.print(__FILE__);    \
+            SERIAL_DB.print(__LINE__);   \
+            SERIAL_DB.print(" error code =");  \
+            SERIAL_DB.println(a);                   \
+            return a;   \
+        }}while(0)
 
 #endif
 
@@ -61,53 +60,50 @@ typedef enum
 
 #define INTEGRA_BIT         0X0C
 
-/**Divide UV index into five levels:LOW,MODERATE,HIGH,VERY_HIGH,EXTREME.
- * 
+/** Divide UV index into five levels:LOW,MODERATE,HIGH,VERY_HIGH,EXTREME.
+
 */
-typedef enum 
-{
-    LOW_LEVEL=0,
-    MODERATE, 
+typedef enum {
+    LOW_LEVEL = 0,
+    MODERATE,
     HIGH_LEVEL,
-    VERY_HIGH, 
+    VERY_HIGH,
     EXTREME,
 } RISK_LEVEL;
 
 
 /** Tntegration time,When Rset =300kΩ，T=125ms，Rset = 600KΩ,T=-250ms
- * 
+
  * */
-typedef enum
-{
-    HALF_OF_T=0X00,
-    ONE_T=0X04,
-    TWO_T=0X08,
-    FOUR_T=0X0C,
-}integration_time_t;
+typedef enum {
+    HALF_OF_T = 0X00,
+    ONE_T = 0X04,
+    TWO_T = 0X08,
+    FOUR_T = 0X0C,
+} integration_time_t;
 
 
 
-class VEML6070
-{
-    public:
-        VEML6070();
-        ~VEML6070(){}
-        err_t clear_ack();
-        err_t set_cmd_reg();
-        err_t read_step(u16 &step);
+class VEML6070 {
+  public:
+    VEML6070();
+    ~VEML6070() {}
+    err_t clear_ack();
+    err_t set_cmd_reg();
+    err_t read_step(u16& step);
 
-        err_t init();
-        err_t disable();
-        err_t enable();
-        err_t set_interrupt(bool thre,bool stat);
-        err_t set_integra_time(integration_time_t T);
-        void wait_for_ready();
-        RISK_LEVEL convert_to_risk_level(u16 uvs_step);
-    private:
-        u8 addr_h;
-        u8 addr_l;
-        u8 addr_ara;
-        u8 cmd_reg;
+    err_t init();
+    err_t disable();
+    err_t enable();
+    err_t set_interrupt(bool thre, bool stat);
+    err_t set_integra_time(integration_time_t T);
+    void wait_for_ready();
+    RISK_LEVEL convert_to_risk_level(u16 uvs_step);
+  private:
+    u8 addr_h;
+    u8 addr_l;
+    u8 addr_ara;
+    u8 cmd_reg;
 };
 
 
--- examples/basic_demo/basic_demo.ino
+++ examples/basic_demo/basic_demo.ino
@@ -3,25 +3,24 @@
 
 /*SAMD core*/
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
-  #define SERIAL SerialUSB
+    #define SERIAL SerialUSB
 #else
-  #define SERIAL Serial
+    #define SERIAL Serial
 #endif
 
 VEML6070 sensor;
 
-char *UV_str[]={"low level","moderate level","high_level","very high","extreme"};
+char* UV_str[] = {"low level", "moderate level", "high_level", "very high", "extreme"};
 
-err_t read_UV()
-{
-    err_t ret=NO_ERROR;
+err_t read_UV() {
+    err_t ret = NO_ERROR;
     u16 step;
     sensor.wait_for_ready();
-    CHECK_RESULT(ret,sensor.read_step(step));
+    CHECK_RESULT(ret, sensor.read_step(step));
     SERIAL.print("UV step = ");
     SERIAL.println(step);
-    RISK_LEVEL level=sensor.convert_to_risk_level(step);
-    
+    RISK_LEVEL level = sensor.convert_to_risk_level(step);
+
     SERIAL.print("UV level is ");
     SERIAL.println(UV_str[level]);
     SERIAL.println(" ");
@@ -30,24 +29,20 @@ err_t read_UV()
     return ret;
 }
 
-void setup()
-{
+void setup() {
     SERIAL.begin(115200);
     delay(10);
     SERIAL.println("serial start!!");
     delay(1000);
-    if(sensor.init())
-    {
-      SERIAL.println("init failed!!!");
+    if (sensor.init()) {
+        SERIAL.println("init failed!!!");
     }
 }
 
 
-void loop()
-{
-    if(read_UV())
-    {
-      SERIAL.print("read UV sensor failed!!");
+void loop() {
+    if (read_UV()) {
+        SERIAL.print("read UV sensor failed!!");
     }
     //sensor.enable();
     //sensor.disable();
